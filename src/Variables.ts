666c71ac0ad16c6c4634e091376b3b21c38d0675
Angie <angelicazamudiocamargo@gmail.com>
2023-09-20 17:54:42 -0600

    Remade project, starting from zero. Plus variables

---
+++ src/Variables.ts
@@ -0,0 +1,27 @@
+ /*Main colors*/
+ export const mainBlue = "#2a7ae4";
+
+ /*Course colors*/
+ export const lightBlue = "#6bd1ff";
+ export const lightGreen="#9cd33b";
+ export const darkGreen = "#00c86f";
+ export const purple = "#6b5be2";
+ export const yellow = "#ffba05";
+ export const orange = "#ff8c2a";
+ export const pink = "#dc6ebe";
+ 
+ /*Blacks*/
+ export const blackDark = "rgba(0, 0, 0, 0.9)";
+ export const blackLight = "rgba(0, 0, 0, 0.6)";
+ export const blackLighter = "rgba(0, 0, 0, 0.5)";
+ export const blackMedium = "rgba(158, 158, 158, 1)";
+ 
+ /*Grays*/
+ export const grayDark = "rgba(194, 194, 194, 1)";
+ export const grayMedium = "rgba(229, 229, 229, 1)";
+ export const grayLight = "rgba(245, 245, 245, 1)";
+ 
+ /*Errors*/
+ export const errorDark = "rgba(198, 40, 40, 1)";
+ export const errorMedium = "rgba(229, 57, 53, 1)";
+ export const errorLight = "rgba(252, 231, 231, 1)";
